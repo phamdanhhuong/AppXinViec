f200a47f9e10a9bfc0f4b0b63e1bafe5bdcc2182
unknown <hieu7a3thcsbt@gmail.com>
2024-04-15 20:59:22 +0700

    ucAppliedJob

---
+++ AppXinViecWPF/View/Applicant/UCIconAppliedJobs.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AppXinViecWPF.View.Applicant
+{
+    /// <summary>
+    /// Interaction logic for UCIconAppliedJobs.xaml
+    /// </summary>
+    public partial class UCIconAppliedJobs : UserControl
+    {
+        public UCIconAppliedJobs()
+        {
+            InitializeComponent();
+        }
+
+        private void btnViewCV_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnChat_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}

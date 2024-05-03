03fefba23e23a32e4d6640f41d79f0e5d2eba515
hieu <hieu7a3thcsbt@gmail.com>
2024-05-03 22:02:22 +0700

    chonNgayPhongvan

---
+++ AppXinViecWPF/View/Applicant/UCChoseInterviewDay.xaml.cs
@@ -0,0 +1,38 @@
+﻿using AppXinViecWPF.DAO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AppXinViecWPF.View.Applicant
+{
+    /// <summary>
+    /// Interaction logic for UCChoseInterviewDay.xaml
+    /// </summary>
+    public partial class UCChoseInterviewDay : Window
+    {
+        public UCChoseInterviewDay()
+        {
+            InitializeComponent();
+            /*
+            int[] days = CVDAO.Instance.GetAllDay(AccountDAO.UserID);
+            foreach (int day in days)
+            {
+                RadioButton a = new RadioButton();
+                a.VerticalAlignment = VerticalAlignment.Center;
+                a.Content = new UCDayInterviewPreview(id);
+                a.Checked += RadioButton_Checked;
+                listDay.Items.Add(a);
+            }*/
+        }
+    }
+}
